8e19cd81d1e1c06fbe7e458b19dcf736aad88686
Reed Hambrook <reed.hambrook@gmail.com>
2018-04-08 16:39:18 -0600

    Added provide statement

--- boolean-evaluator.rkt
+++ boolean-evaluator.rkt
@@ -1,8 +1,10 @@
 #lang racket
+(provide evalBoolean)
+
 
 ; Takes a list of format '(number conditional number) e.g. '(2 "<" 4)
 ; Conditionals are strings
-(define (booleanEval expr)
+(define (evalBoolean expr)
   (case (cadr expr)
     [("==") (eq? (car expr) (car (cddr expr)))]
     [("<>") (not (eq? (car expr) (car (cddr expr))))]
