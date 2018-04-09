ece2abf1ffbb209355ebf49a5b0e1938dfc80d68
Nicolas Enslen <nic.enslen@uleth.ca>
2018-04-09 03:20:31 +0000

    Implemented #definevari

--- command.rkt
+++ command.rkt
@@ -1,11 +1,16 @@
 #lang racket
+(require "variables.rkt")
 (provide handle-define-vari)
 (provide handle-exit)
 (provide handle-clear)
 (provide handle-define-function)
 
 
-(define (handle-define-vari tokens) "define the handle-define-vari handler here")
+(define (handle-define-vari tokens)
+  (defineVar (list-ref tokens 1) (list-ref tokens 2) 0)
+  "Variable added"
+)
+
 (define (handle-exit tokens) "define the handle-exit handler here")
 (define (handle-clear tokens) "define the handle-clear handler here")
 
