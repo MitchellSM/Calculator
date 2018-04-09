ca0dd6e2e3d3b420a7b11c8b27db97a076800104
Brad Lindsay <br.mi.lind@gmail.com>
2018-04-08 18:36:46 -0600

    everything should be working...ish

--- command.rkt
+++ command.rkt
@@ -1,4 +1,13 @@
 #lang racket
+(provide handle-define-vari)
+(provide handle-exit)
+(provide handle-clear)
 (provide handle-define-function)
 
+
+(define (handle-define-vari tokens) "define the handle-define-vari handler here")
+(define (handle-exit tokens) "define the handle-exit handler here")
+(define (handle-clear tokens) "define the handle-clear handler here")
+
 (define (handle-define-function tokens) "mitch work here")
+
