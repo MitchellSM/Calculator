5958c778c487a8e42828cf3f065069d371b5908e
nenslen <nicolas.enslen@gmail.com>
2018-04-10 01:44:19 -0600

    Updated formatting

--- shunting-yard.rkt
+++ shunting-yard.rkt
@@ -2,9 +2,11 @@
 (require "helpers.rkt")
 (provide (all-defined-out))
 
-; Determines the associativity of a given operator
+; Gets the associativity of a given operator
 ; operator = An operator, represented as a char
-(define (associativity-of operator)
+(define (get-associativity operator)
+
+  ; Check the associativity of the operator
   (if (member operator '(#\+ #\- #\* #\/ #\%))
       'left
       'right
@@ -14,7 +16,7 @@
 
 ; Determines the precedence of a given operator
 ; operator = An operator, represented as a char
-(define (precedence-of operator)
+(define (get-precedence operator)
   (case operator
     ((#\= #\< #\>) 1)
     ((#\+ #\-)     2)
@@ -25,42 +27,35 @@
 )
 
 
-; Returns the last element of a list
-; lst = A list
-(define (last lst)
-  (if (null? (cdr lst))
-      (car lst)
-      (last (cdr lst))
-   )
-)
-
-
-; Checks if a given character is numerical
-(define (digit-char? char)
-  (> 10
-     (- (char->integer char) (char->integer #\0))
-     (- 1)
-  )
-)
-
-
-; Actions to take if the token in the stmt is an operator
+; Special operator conditions
 (define (operator-actions stmt stack)
-  (let* ((token-precedence (precedence-of (car stmt)))
-         (token-assoc (associativity-of (car stmt)))
+  (let* (
+         ; Get the token's precedence
+         (token-precedence (get-precedence (car stmt)))
+
+         ; Get the token's associativity
+         (token-assoc (get-associativity (car stmt)))
+
+         ; Get the stack operator
          (stack-oper
             (if (not (null? stack))
                 (car stack)
                 '()
             )
          )
+
+         ; Get the stack operator's precedence
          (stack-precedence
             (if (not (null? stack-oper))
-                (precedence-of stack-oper)
+                (get-precedence stack-oper)
                 0
             )
-         )
-         )
+         ))
+
+    ; Check the following conditions and pop operators from stack
+    ; Is the op at the top of the stack with greater precedence?
+    ; Is the op at the top of the stack equal precedence and left associative?
+    ; Is the op at the top of the stack not a left bracket?
     (cond ((or (and (eq? token-assoc 'left)
                     (<= token-precedence stack-precedence))
                (and (eq? token-assoc 'right)
@@ -71,31 +66,36 @@
 )
 
 
-; Actions to take if (null? stmt)
+; Check if statement is null
 (define (stack-operations stack)
-  ;; If a left-parenthesis is found on the stack,
-  ;; it means there was no right-parenthesis to match it
-  ;; and thus the statement has unbalanced parentheses.
   (cond ((and (not (null? stack))
               (eq? (car stack) #\())
-         (display "Unbalanced parenthesis"))
+         (display "Incorrect format"))
         ((null? stack) '())
-        (else (cons (car stack) (%shunting-yard '() (cdr stack))))))
+        (else (cons (car stack) (%shunting-yard '() (cdr stack))))
+  )
+)
 
 
-;; Implementation of Dijkstra's Shunting-yard Algorithm
-; "Converts infix-notation mathematical equations into
-;postfix-notation mathematical equations, using an
-;implementation of Dijkstra's Shunting-yard Algorithm."
+; The shunting-yard algorithm takes an infix expression and converts it
+; to a postfix expression
 (define (%shunting-yard stmt stack)
   (cond ((null? stmt)
          (stack-operations stack))
+
+        ; Numbers always go onto the stack
         ((number? (car stmt))
          (cons (car stmt) (%shunting-yard (cdr stmt) stack)))
+
+        ; Operators have special conditions
         ((operator? (car stmt))
          (operator-actions stmt stack))
+
+        ; Left brackets always go onto the stack
         ((eq? (car stmt) #\()
          (%shunting-yard (cdr stmt) (cons (car stmt) stack)))
+
+        ; Right brackets require us to pop until we find a matching left bracket
         ((eq? (car stmt) #\))     
          (if (eq? #\( (car stack))
              (%shunting-yard (cdr stmt) (cdr stack))
